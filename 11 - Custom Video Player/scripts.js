687c021d3f1ec71aa051706619eb626f58270bb2
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-02-19 19:43:21 -0700

    Change volume when moving volume slider

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,11 +1,16 @@
 const player = document.querySelector('video.player__video.viewer')
+
 const playerControl = document.querySelector('.player__controls')
 const playButton = playerControl.querySelector('button.player__button')
+const volumeSlider = playerControl.querySelector('input[name=volume].player__slider')
 
 let isPlaying = false
 
 player.addEventListener('click', handlePlayClick)
 playButton.addEventListener('click', handlePlayClick)
+
+volumeSlider.addEventListener('change', handleVolume)
+volumeSlider.addEventListener('mousemove', handleVolume)
 
 function handlePlayClick (event) {
   if (!isPlaying) {
@@ -18,4 +23,8 @@ function handlePlayClick (event) {
   }
 
   isPlaying = !isPlaying
+}
+
+function handleVolume (event) {
+  player.volume = this.value
 }
