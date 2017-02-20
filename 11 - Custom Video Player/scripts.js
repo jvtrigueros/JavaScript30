c8be013554f8e6194edde53eca81eb9337a9c7fb
Jose V. Trigueros <j.v.trigueros@gmail.com>
2017-02-19 19:47:48 -0700

    Change playback rate using playback slider

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -3,6 +3,7 @@ const player = document.querySelector('video.player__video.viewer')
 const playerControl = document.querySelector('.player__controls')
 const playButton = playerControl.querySelector('button.player__button')
 const volumeSlider = playerControl.querySelector('input[name=volume].player__slider')
+const playbackSlider = playerControl.querySelector('input[name=playbackRate].player__slider')
 
 let isPlaying = false
 
@@ -11,6 +12,9 @@ playButton.addEventListener('click', handlePlayClick)
 
 volumeSlider.addEventListener('change', handleVolume)
 volumeSlider.addEventListener('mousemove', handleVolume)
+
+playbackSlider.addEventListener('change', handlePlayback)
+playbackSlider.addEventListener('mousemove', handlePlayback)
 
 function handlePlayClick (event) {
   if (!isPlaying) {
@@ -27,4 +31,8 @@ function handlePlayClick (event) {
 
 function handleVolume (event) {
   player.volume = this.value
+}
+
+function handlePlayback (event) {
+  player.playbackRate = this.value
 }
